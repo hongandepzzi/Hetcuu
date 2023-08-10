eab228c1727413ea2a0ed6438a78d5955dd1e729
hongandepzzi <94431987+hongandepzzi@users.noreply.github.com>
2023-08-10 14:46:08 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    alert("Bố mày gầy");
+  }
+function muFunction() {
+    alert("Me May Beo");
+}
+function miFunction() {
+    alert("sao may beo vay tran=)))")
+}
+function mbFunction() {
+    alert("Bấm vào cái hình á")
+}
+function mcFunction() {
+    alert("Thấy đẹp trai quá mới xin in4 đúng không=)))")
+}
+let name = "An"
+console.log("Hello" + " " + name)
